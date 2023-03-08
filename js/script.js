ee1a4473bc8923bc958d497a9137640c2842ec0c
Shamim-Dewan <570iamkhan@gmail.com>
2023-03-08 09:49:11 +0600

    completed funtionality

--- js/script.js
+++ js/script.js
@@ -29,3 +29,55 @@ let price = document.getElementById("mobile-price");
 let priceCalculate = decreamentCountNum * 1219;
 price.innerText = priceCalculate;
 })
+
+// second cart
+
+// btn-plus
+document.getElementById("btn-Plus").addEventListener("click", function(){
+    let counter = document.getElementById("display-number");
+    let countNum = parseFloat(counter.value);
+    let increamentCountNum = countNum + 1;
+    counter.value = increamentCountNum;
+
+    // price count
+let price = document.getElementById("cover-price");
+let priceCalculate = increamentCountNum * 1359;
+price.innerText = priceCalculate;
+})
+
+// btn-minus
+document.getElementById("btn-Minus").addEventListener("click", function(){
+  let counter = document.getElementById("display-number");
+  let countNum = parseFloat(counter.value);
+  let decreamentCountNum = countNum - 1;
+   // if less than 1
+   if(decreamentCountNum < 1){
+    alert("You have to add atleast One item");
+    counter.value = decreamentCountNum + 1;
+    return;
+  }
+  counter.value = decreamentCountNum;
+
+// price count
+let price = document.getElementById("cover-price");
+let priceCalculate = decreamentCountNum * 1359;
+price.innerText = priceCalculate;
+})
+
+// total
+document.getElementById("btn-checkout").addEventListener("mouseover",function(){
+  let Fistprice = +document.getElementById("cover-price").innerText;
+  let Secondprice = +document.getElementById("mobile-price").innerText;
+// subtotal
+  let subTotal = document.getElementById("sub-total");
+  let subTotalCalc = Fistprice + Secondprice;
+  subTotal.innerText = subTotalCalc;
+// tax
+let tax = document.getElementById("tax");
+let vat = subTotalCalc * 0.15;
+tax.innerText = vat;
+// total
+let total = document.getElementById("total");
+let totalCalc = subTotalCalc + vat;
+total.innerText = totalCalc;
+})
